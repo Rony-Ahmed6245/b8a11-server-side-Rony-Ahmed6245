81b4bf54675e304cbcceb2cd31149a988a562b02
ronyahmed6245 <ronyahmed6245.gmail.com>
2023-11-08 17:02:57 +0600

    change path with version control system

--- index.js
+++ index.js
@@ -8,15 +8,15 @@ const port = process.env.PORT || 5001;
 
 
 
-// middleware
+// middleware==>
 app.use(cors());
 app.use(express.json());
 
 
 
 // user name and pass  uri 
-console.log(process.env.DB_USER);
-console.log(process.env.DB_PASS);
+// console.log(process.env.DB_USER);
+// console.log(process.env.DB_PASS);
 // const uri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.eehyjj4.mongodb.net/<database-name>?retryWrites=true&w=majority`;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.eehyjj4.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri);
@@ -54,11 +54,6 @@ async function run() {
       });
 
 
-
-
-
-
-
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     // Send a ping to confirm a successful connection
@@ -78,5 +73,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Simple Crud is Running on port ${port}`);
+  console.log(`Job server Running on port ${port}`);
 });
